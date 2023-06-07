b75375063fed33a8c14827b6c6872604b4c41fad
dantb <dantb@hotmail.co.uk>
2023-06-07 15:59:47 +0100

    Messing around

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,4 +1,23 @@
 package sandbox
 
+import cats.syntax.all.*
+import cats.kernel.Semigroup
+
+final case class Box(value: Int)
+
+given Semigroup[Box] = (a, b) => Box(a.value + b.value)
+
 @main def main(): Unit =
-  println("Hello world!")
+  println(List(1, 2, 3) |+| List(4, 5))
+  println(Option(1) |+| none)
+  val thing: Int => String = x => s"hello$x"
+  val thing2: Int => String = x => s"world$x"
+  val thing3 = thing |+| thing2
+  // val thing4 = thing |+| egg
+  println(thing3(4))
+  println(("x", 4.3) |+| ("y", 7.8))
+  println(Map("a" -> 1, "b" -> 3) |+| Map("a" -> 2))
+
+
+  
+
